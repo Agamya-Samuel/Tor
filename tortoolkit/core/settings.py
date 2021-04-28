f76b4d801ab01ffac9f7af8995b6408e60f6d3fa
AYUSH <81183831+ayushteke@users.noreply.github.com>
2021-04-28 16:41:44 +0530

    Update settings.py

--- tortoolkit/core/settings.py
+++ tortoolkit/core/settings.py
@@ -22,7 +22,7 @@ no = "❌"
 yes = "✅"
 # Central object is not used its Acknowledged 
 tordb = TorToolkitDB()
-header =  '<b>**TorToolKit** by <a> href="https://github.com/ayushteke">AYUSH</a></b>\n<u>ADMIN SETTINGS MENU - Beta v1</u>'
+header =  '<b>Leech BOT by <a href="https://t.me/AT_BOTS">@AT_BOTS</a></b>\n<u>ADMIN SETTINGS MENU - Beta v1</u>'
 async def handle_setting_callback(e):
     db = tordb
     session_id,_ = db.get_variable("SETTING_AUTH_CODE")
